9a21ec5028bf458ffc0c1369e3332298cd2f4a3b
Rick Zakharov <rick.zakharov@gmail.com>
2024-06-07 20:46:28 -0400

    Added k8s cleanup script

--- kafka-consumer-iceberg/src/main/java/com/example/KafkaConsumerIceberg.java
+++ kafka-consumer-iceberg/src/main/java/com/example/KafkaConsumerIceberg.java
@@ -1,17 +1,21 @@
 package com.example;
 
-// import java.util.List;
-// import java.util.stream.Collectors;
-// import org.apache.flink.api.common.typeinfo.TypeInformation;
-// import org.apache.flink.api.java.typeutils.RowTypeInfo;
-
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.formats.json.JsonDeserializationSchema;
+import org.apache.flink.formats.json.JsonSerializationSchema;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import static org.apache.flink.table.api.Expressions.$;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -19,11 +23,14 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.Properties;
 
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+
+import java.io.InputStream;
 
 public class KafkaConsumerIceberg {
     private static Logger logger = LoggerFactory.getLogger(KafkaConsumerIceberg.class);
@@ -34,7 +41,6 @@ public class KafkaConsumerIceberg {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
 
-        props.put("group.id", "test");
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
@@ -79,32 +85,74 @@ public class KafkaConsumerIceberg {
         tableEnv.useCatalog("iceberg");
 
         // Create a database in the current catalog
-        tableEnv.executeSql("CREATE DATABASE IF NOT EXISTS db");
+        tableEnv.executeSql("CREATE DATABASE IF NOT EXISTS real-estate");
 
         // create the table
         tableEnv.executeSql(
-                "CREATE TABLE IF NOT EXISTS db.table1 ("
+                "CREATE TABLE IF NOT EXISTS real-estate.assessments ("
                         + "id BIGINT COMMENT 'unique id',"
                         + "data STRING"
                         + ")");
 
-        // create a DataStream of Tuple2 (equivalent to Row of 2 fields)
-        DataStream<Tuple2<Long, String>> dataStream = env.fromElements(
-                Tuple2.of(1L, "foo"),
-                Tuple2.of(1L, "bar"),
-                Tuple2.of(1L, "baz"));
 
-        // convert the DataStream to a Table
-        Table table = tableEnv.fromDataStream(dataStream, $("id"), $("data"));
+        Properties consumerConfig = new Properties();
+        try (InputStream stream = FlightImporterJob.class.getClassLoader().getResourceAsStream("consumer.properties")) {
+                consumerConfig.load(stream);
+        }
 
-        // register the Table as a temporary view
-        tableEnv.createTemporaryView("my_datastream", table);
+        Properties producerConfig = new Properties();
+        try (InputStream stream = FlightImporterJob.class.getClassLoader().getResourceAsStream("producer.properties")) {
+                producerConfig.load(stream);
+        }
 
-        // write the DataStream to the table
-        tableEnv.executeSql(
-                "INSERT INTO db.table1 SELECT * FROM my_datastream");
+        KafkaSource<SkyOneAirlinesFlightData> skyOneSource = KafkaSource.<SkyOneAirlinesFlightData>builder()
+            .setProperties(consumerConfig)
+            .setTopics("skyone")
+            .setStartingOffsets(OffsetsInitializer.latest())
+            .setValueOnlyDeserializer(new JsonDeserializationSchema(SkyOneAirlinesFlightData.class))
+            .build();
 
-        env.execute("Flink Streaming Java API Skeleton");
+        DataStream<SkyOneAirlinesFlightData> skyOneStream = env
+            .fromSource(skyOneSource, WatermarkStrategy.noWatermarks(), "skyone_source");
+
+        KafkaRecordSerializationSchema<FlightData> flightSerializer = KafkaRecordSerializationSchema.<FlightData>builder()
+            .setTopic("flightdata")
+            .setValueSerializationSchema(new JsonSerializationSchema<FlightData>(
+                () -> {
+                    return new ObjectMapper()
+                        .registerModule(new JavaTimeModule());
+                }
+            ))
+            .build();
+
+        KafkaSink<FlightData> flightSink = KafkaSink.<FlightData>builder()
+            .setKafkaProducerConfig(producerConfig)
+            .setRecordSerializer(flightSerializer)
+            .build();
+
+        defineWorkflow(skyOneStream)
+            .sinkTo(flightSink)
+            .name("flightdata_sink");
+
+        env.execute("FlightImporter");
+
+        // // create a DataStream of Tuple2 (equivalent to Row of 2 fields)
+        // DataStream<Tuple2<Long, String>> dataStream = env.fromElements(
+        //         Tuple2.of(1L, "foo"),
+        //         Tuple2.of(1L, "bar"),
+        //         Tuple2.of(1L, "baz"));
+
+        // // convert the DataStream to a Table
+        // Table table = tableEnv.fromDataStream(dataStream, $("id"), $("data"));
+
+        // // register the Table as a temporary view
+        // tableEnv.createTemporaryView("my_datastream", table);
+
+        // // write the DataStream to the table
+        // tableEnv.executeSql(
+        //         "INSERT INTO db.table1 SELECT * FROM my_datastream");
+
+        // env.execute("Flink Streaming Java API Skeleton");
     }
 
     private static void testConsumeFromKafka(KafkaConsumer<String, String> consumer) {
