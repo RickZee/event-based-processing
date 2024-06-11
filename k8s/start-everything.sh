8cbc94e49aa13d81a3206afec62eb0173f88af7d
Rick Zakharov <rick.zakharov@gmail.com>
2024-06-11 15:00:39 -0400

    Everything in k8s is now running!

--- k8s/start-everything.sh
+++ k8s/start-everything.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 kubectl apply -f namespaces.yaml
+sleep 10
 kubectl apply -f cert-manager.yaml
+sleep 10
 kubectl apply -f postgresql-statefulset.yaml
+sleep 10
 kubectl apply -f pgadmin-statefulset.yaml
+sleep 10
 kubectl apply -f debezium-deployment.yaml
+sleep 10
 kubectl apply -f kafka-operator.yaml
+sleep 10
 kubectl apply -f kafka-single-node.yaml
+sleep 10
 kubectl apply -f kafka-topic.yaml
+sleep 10
+kubectl apply -f kafka-ui-deployment.yaml
+sleep 10
 kubectl apply -f flink-operator.yaml
+sleep 10
 kubectl apply -f flink-cluster.yaml
+sleep 10
 kubectl apply -f minio-operator.yaml
+sleep 10
 kubectl apply -f minio-tenant.yaml
+sleep 10
